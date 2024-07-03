1cc6ecbbb30bb31688ef16f6842335503664f14a
jhleemax <jhleemax@gmail.com>
2024-07-04 00:20:57 +0900

    (NEW) splash, login activity

--- app/src/main/java/com/yobi/register/Activity_login.java
+++ app/src/main/java/com/yobi/register/Activity_login.java
@@ -1,4 +1,4 @@
-package com.yobi;
+package com.yobi.register;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.yobi.R;
+
+public class Activity_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
