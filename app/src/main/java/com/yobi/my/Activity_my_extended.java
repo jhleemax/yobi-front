4b7abfa6709b595642edb343dfa9ce7bf8f4c591
jhleemax <jhleemax@gmail.com>
2024-08-06 17:27:31 +0900

    (화면추가) 설정창, 내 게시물 조회, 커뮤니티 작성, 커뮤니티 상세조회, (이벤트, 알림, 찜게시물 조회, 최근에 본 레시피, 공지사항)

---
+++ app/src/main/java/com/yobi/my/Activity_my_extended.java
@@ -0,0 +1,26 @@
+package com.yobi.my;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.yobi.R;
+
+public class Activity_my_extended extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_my_extended);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/yobi/my/Fragment_my_boards.java
+++ app/src/main/java/com/yobi/my/Fragment_my_boards.java
@@ -3,22 +3,19 @@ package com.yobi.my;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.yobi.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_my#newInstance} factory method to
+ * Use the {@link Fragment_my_boards#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_my extends Fragment {
+public class Fragment_my_boards extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,17 +25,8 @@ public class Fragment_my extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    // Fragments
-    Fragment_my_boards fragment_my_boards;
 
-    // 컴포넌트
-    LinearLayout myBoards;
-
-    // 데이터 or Manager
-    FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
-
-    public Fragment_my() {
+    public Fragment_my_boards() {
         // Required empty public constructor
     }
 
@@ -48,11 +36,11 @@ public class Fragment_my extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_my.
+     * @return A new instance of fragment Fragment_my_boards.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment_my newInstance(String param1, String param2) {
-        Fragment_my fragment = new Fragment_my();
+    public static Fragment_my_boards newInstance(String param1, String param2) {
+        Fragment_my_boards fragment = new Fragment_my_boards();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -72,33 +60,7 @@ public class Fragment_my extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_my, container, false);
-
-        // 초기화
-        myBoards = v.findViewById(R.id.linearLayout_my_board);
-
-        fragment_my_boards = new Fragment_my_boards();
-
-        // 이벤트
-        myBoards.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFrag(0);
-            }
-        });
-
         // Inflate the layout for this fragment
-        return v;
-    }
-
-    private void setFrag(int n) {
-        fragmentManager = getFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-
-        switch (n) {
-            case 0:
-                fragmentTransaction.replace(R.id.frameLayout_main, fragment_my_boards).commit();
-                break;
-        }
+        return inflater.inflate(R.layout.fragment_my_boards, container, false);
     }
 }
